9c0215620a9cd3d1a24798b0f99acb2b190134b5
Tilly-Alton <82646699+Tilly-Alton@users.noreply.github.com>
2023-10-24 16:27:02 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,5 @@
-/* ENGR202: pwm output using nucleo
- * Originally Ekky Hermestian IW
- * Tilly Alton 16/10/2023
- */
-
-// This code causes the built in LED to blink. The actual code is written
-// outside the while loop but it is executed by calling the function blink
 #include "mbed.h"
 
-// Built-in LED output (also visible via oscilloscope, pin D13)
 PwmOut mypwm(PB_4);
 DigitalOut myled(LED1);
 
